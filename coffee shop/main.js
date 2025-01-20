844d4e7b884eddc5b2bf2f05baf2577e542f34e7
marko-codex <markokalamashviliroque@gmail.com>
2025-01-20 17:05:30 +0400

    Add files via upload

---
+++ coffee shop/main.js
@@ -0,0 +1,45 @@
+// Swiper
+
+var swiper = new Swiper(".mySwiper", {
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
+
+
+// menu open close
+let menu = document.querySelector('.menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () => {
+  menu.classList.toggle("move")
+  navbar.classList.toggle('open-menu')
+};
+
+// Close menu on Scroll!
+window.onscroll = () => {
+  menu.classList.remove("move")
+  navbar.classList.remove('open-menu')
+};
+
+// ScrollReveal Animation
+const animate = ScrollReveal({
+  origin: 'top',
+  distance: '60px',
+  duration: '2500',
+  delay: '400',
+});
+
+animate.reveal(".nav");
+animate.reveal(".home-text", {origin: "left"});
+animate.reveal(".home-img", {origin: "bottom"});
+animate.reveal(".ser-box, .product-box, .team-box, .book-data", {interval: 100});
+
+
